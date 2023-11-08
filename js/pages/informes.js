25b2c2fa926029d72c0418f3c409dc0e64b04d19
alanmoex <alan.moex@gmail.com>
2023-11-08 20:52:36 -0300

    persiste error en informes.js

--- js/pages/informes.js
+++ js/pages/informes.js
@@ -1,3 +1,9 @@
+const mensajeAmarillo = document.getElementById('mensaje-amarillo')
+const mensajeVerde = document.getElementById('mensaje-verde')
+const mensajeRojo = document.getElementById('mensaje-rojo')
+const mensajeCargando = document.getElementById('mensaje-cargando')
+const informesContainer = document.getElementById('informe-container')
+
 let informes = [];
 
 let anioEleccion = '';
@@ -10,17 +16,26 @@ let seccionId = '';
 let circuitoId = "";
 let mesaId = "";
 
-document.addEventListener('DOMContentLoaded', () => {
+let añoSeleccionado = ""
+let cargoSeleccionado = ""
+let distritoSeleccionado = ""
+let seccionSeleccionada = ""
+let eleccion = ""
+
+document.addEventListener('DOMContentLoaded', async () => {
     if (localStorage.getItem('INFORMES')) {
         informes = JSON.parse(localStorage.getItem('INFORMES'));
-        console.log(informes);
+        const promises = informes.map(datos => {
+            url = armarUrl(datos);
+            return consultarResultados(url);
+        });
+        await Promise.all(promises);
         informes.forEach(datos => {
-
+            crearInforme();
         });
     } else {
-        mostrarMensaje(mensajeAmarillo, "Debe agregar un INFORME desde Paso o Generales primero!")
+        mostrarMensaje(mensajeAmarillo, "Debe agregar un INFORME desde Paso o Generales primero!");
     }
-
 });
 
 function mostrarMensaje(tipoMensaje, mensaje,) {
@@ -33,7 +48,6 @@ function mostrarMensaje(tipoMensaje, mensaje,) {
 
 function armarUrl(data) {
     let datos = data.split('|');
-    console.log(datos);
 
     anioEleccion = datos[0];
     tipoRecuento = datos[1];
@@ -44,40 +58,40 @@ function armarUrl(data) {
     seccionId = datos[6];
     circuitoId = datos[7];
     mesaId = datos[8];
+    añoSeleccionado = datos[9];
+    cargoSeleccionado = datos[10];
+    distritoSeleccionado = datos[11];
+    seccionSeleccionada = datos[12];
 
-    const urlSinParametros = `https://resultados.mininterior.gob.ar/api/resultados/getResultados`;
+    if (tipoEleccion == 1) {
+        eleccion = "Paso"
+    } else {
+        eleccion = "Generales"
+    }
+
+    let urlSinParametros = `https://resultados.mininterior.gob.ar/api/resultados/getResultados`;
     let parametros = `?anioEleccion=${anioEleccion}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${categoriaId}&distritoId=${distritoId}&seccionProvincialId=${seccionProvincialId}&seccionId=${seccionId}&circuitoId=${circuitoId}&mesaId=${mesaId}`
     let url = urlSinParametros + parametros;
-
+    console.log(url)
     return url;
+
 }
 
-async function consultarResultados() {
-    const url = `https://resultados.mininterior.gob.ar/api/resultados/getResultados`
-    let anioEleccion = periodosSelect.value;
-    let categoriaId = cargosSelect.value;
-    let distritoId = distritosSelect.value;
-    let seccionProvincialId = 0; //tiene que ir 0 porque no reconoce el null
-    let seccionId = seccionSelect.value;
-    let circuitoId = "";
-    let mesaId = "";
-    let parametros = `?anioEleccion=${anioEleccion}&tipoRecuento=${tipoRecuento}&tipoEleccion=${tipoEleccion}&categoriaId=${categoriaId}&distritoId=${distritoId}&seccionProvincialId=${seccionProvincialId}&seccionId=${seccionId}&circuitoId=${circuitoId}&mesaId=${mesaId}`
+async function consultarResultados(url) {
     try {
+        console.log('entra en el try de consultar resultados')
         mensajeCargando.style.visibility = 'visible';
-        const response = await fetch(url + parametros);
+        let response = await fetch(url);
+        console.log('hace el fetch')
         if (response.ok) {
+            console.log('respuesta ok')
             mensajeCargando.style.visibility = 'hidden';
-            resultados = await response.json();
+            const resultados = await response.json();
             console.log(resultados)
-            mostrarTitulos();
-            cambiarImagenProvincia();
-            crearListaAgrupacionesYColores();
-            completarCuadroAgrupaciones();
-            completarResumenVotos();
-            mostrarContenido();
-            cuadroMesas.textContent = `${resultados.estadoRecuento.mesasTotalizadas}`
-            cuadroElectores.textContent = `${resultados.estadoRecuento.cantidadElectores}`
-            cuadroParticipacion.textContent = `${resultados.estadoRecuento.participacionPorcentaje}%`
+            console.log(resultados.valoresTotalizadosPositivos)
+            crearInforme(resultados);
+
+
         } else {
             mostrarMensaje(mensajeRojo, "Error. El servicio esta caido por el momento. Intente mas tarde.")
         }
@@ -86,4 +100,94 @@ async function consultarResultados() {
         mostrarMensaje(mensajeRojo, "Error. El servicio esta caido por el momento. Intente mas tarde.")
     }
 
+}
+
+function crearInforme(resultados) {
+    try {
+        let agrupaciones = resultados.valoresTotalizadosPositivos;
+        const tr = document.createElement('tr');
+
+        const tdProvincia = document.createElement('td');
+        tdProvincia.classList.add('columna-eleccion');
+        cambiarImagenProvincia(tdProvincia);
+
+        const tdEleccion = document.createElement('td');
+
+        const h4Eleccion = document.createElement('h4');
+        h4Eleccion.textContent = `Elecciones ${anioEleccion} | ${eleccion}`;
+
+        const pEleccion = document.createElement('p');
+        pEleccion.classList.add('texto-path');
+        pEleccion.textContent = `${anioEleccion}>${eleccion}>${cargoSeleccionado}>${distritoSeleccionado}>${seccionSeleccionada}`
+
+        tdEleccion.appendChild(h4Eleccion);
+        tdEleccion.appendChild(pEleccion);
+
+
+        const tdCuadritos = document.createElement('td');
+
+        const tdDatos = document.createElement('td');
+
+
+        agrupaciones.forEach(agrupacion => {
+            const p1Partido = document.createElement('p');// crea el <p> donde va el nombre de la lista//
+            p1Partido.textContent = agrupacion.nombreAgrupacion;// agrega el nombre de la lista al <p>//
+
+            const spanP1Partido = document.createElement('span'); //crea el span donve va el porcentaje de votos//
+            const porcentajeVotos = `${agrupacion.votosPorcentaje}%`; // calculo el porcentaje y hago que solo tenga 2 decimales//
+            spanP1Partido.textContent = porcentajeVotos;// agrega el porcentaje//
+            spanP1Partido.classList.add('porcentajes'); //agrega el span a la clase porcentajes//
+
+            const p2Partido = document.createElement('p'); //crea el elemnto <p> que va acontener el span con los votos//
+
+            const spanP2Partido = document.createElement('span');// crea el span donve van los votos//
+            spanP2Partido.textContent = `${agrupacion.votos} votos`; // formateo los votos para que se vean asi "XXX votos" //
+            spanP2Partido.classList.add('porcentajes');
+
+            tdDatos.appendChild(p1Partido);
+            tdDatos.appendChild(p2Partido);
+
+            p1Partido.appendChild(spanP1Partido);
+            p2Partido.appendChild(spanP2Partido);
+        });
+
+        tr.appendChild(tdProvincia);
+        tr.appendChild(tdEleccion);
+        tr.appendChild(tdCuadritos);
+        tr.appendChild(tdDatos);
+    } catch (error) {
+        console.log(resultados.valoresTotalizadosPositivos)
+        console.log("No se creo el informe porque el resultado esta vacio")
+    }
+
+
+
+
+
+
+}
+
+function ocultarMensajes() {
+    mensajeCargando.style.visibility = 'hidden'
+    mensajeRojo.style.visibility = 'hidden';
+    mensajeAmarillo.style.visibility = 'hidden';
+    mensajeVerde.style.visibility = 'hidden';
+}
+
+
+function cambiarImagenProvincia(svgContainer) {
+    const provincia = provinciasSVG.find(
+
+        (item) => item.provincia.toUpperCase() === distritoSeleccionado.toUpperCase()
+    );
+
+    if (provincia) {
+        const divSvg = document.createElement('div');
+        divSvg.innerHTML = provincia.svg;
+        divSvg.classList.add('contenedor-provincia');
+
+        svgContainer.appendChild(divSvg);
+    } else {
+        svgContainer.innerHTML = "<p>La imagen no se pudo cargar</p>";
+    }
 }
